3b5eab39d24fe671709007fe40d072c99d38610b
alemapnil <alemapnil@gmail.com>
2021-12-12 22:01:09 +0800

    change name and add chromedriver

---
+++ Django_book_search/myapp/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+class student(models.Model):
+    cName = models.CharField(max_length=20, null=False)
+    cSex = models.CharField(max_length=2, default='M', null=False)
+    cBirthday = models.DateField(null=False)
+    cEmail = models.EmailField(max_length=100, blank=True, default='')
+    cPhone = models.CharField(max_length=50, blank=True, default='')
+    cAddr = models.CharField(max_length=255, blank=True, default='')
+    def __str__(self):
+        return self.cName
